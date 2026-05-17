c6704253dde7e06ac17eed0b22e8fd825916b80e
aleksandrov_va <poiskov89@mail.ru>
2026-05-18 00:36:42 +0300

    добален файл entities.py
    файл содержит описание сущности "событие"

---
+++ app/domain/entities.py
@@ -0,0 +1,20 @@
+class Event:
+    def __init__(self, event_id: int, start_timestamp: int, end_timestamp: int, title: str, description: str):
+        self.id  = event_id
+        self.title = title
+        self.start_at = start_timestamp
+        self.end_at   = end_timestamp
+        self.description = description
+
+    @classmethod
+    def create(cls, event_id: int, start_timestamp: int, end_timestamp: int, title: str, description: str):
+        return cls(event_id, start_timestamp, end_timestamp, title, description)
+
+    def __str__(self):
+        return (f'id: {self.id} \n'
+                f'title: {self.title} \n'
+                f'start_at: {self.start_at}\n'
+                f'end_at: {self.end_at}')
+
+test = Event.create(3, 1317, 1318, 'заголовок события', 'текст события')
+print(test)
